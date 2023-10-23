f753ae316ae2ca600a362f72ae178c65813d4a5b
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-10-23 11:59:39 +0530

    first commit

---
+++ src/main/java/com/example/dassample/DasSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dassample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dassample/DasSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dassample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
